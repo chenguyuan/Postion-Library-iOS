3332721a98ee72f5b9addfd37ef7940409c88e6e
yutianqiuhao <yutianqiuhao@gmail.com>
2014-09-18 13:47:19 +0800

    Initial Commit

---
+++ YCIndoorLocation/YCIndoorLocation.h
@@ -0,0 +1,19 @@
+//
+//  YCIndoorLocation.h
+//  YCIndoorLocation
+//
+//  Created by yutian on 14-9-18.
+//  Copyright (c) 2014年 YongChang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YCIndoorLocation.
+FOUNDATION_EXPORT double YCIndoorLocationVersionNumber;
+
+//! Project version string for YCIndoorLocation.
+FOUNDATION_EXPORT const unsigned char YCIndoorLocationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YCIndoorLocation/PublicHeader.h>
+
+
---
+++ YCIndoorLocationTests/YCIndoorLocationTests.m
@@ -0,0 +1,40 @@
+//
+//  YCIndoorLocationTests.m
+//  YCIndoorLocationTests
+//
+//  Created by yutian on 14-9-18.
+//  Copyright (c) 2014年 YongChang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface YCIndoorLocationTests : XCTestCase
+
+@end
+
+@implementation YCIndoorLocationTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
